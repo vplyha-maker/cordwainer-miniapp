ce22a5cc64e307133000ac88f71107ddf8c8bb6d
vplyha-maker <vplyha@gmail.com>
2026-09-15 06:22:40 +0300

    Add SmoothScroll component with ReactLenis

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,1 +1,22 @@
+import { ReactLenis } from 'lenis/react'
+import { ReactNode } from 'react'
+
+interface SmoothScrollProps {
+  children: ReactNode
+}
+
+export default function SmoothScroll({ children }: SmoothScrollProps) {
+  return (
+    <ReactLenis 
+      root 
+      options={{
+        lerp: 0.07, // Идеальное значение для "дорогого" скролла
+        duration: 1.2,
+        smoothWheel: true,
+      }}
+    >
+      {children}
+    </ReactLenis>
+  )
+}
 
